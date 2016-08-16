67bb4f1cafc8bff19a1ebfb39c8901ea6f71e135
Jonathan Boulanger <Jonathan_Boulanger@Hotmail.com>
2016-08-15 21:48:10 -0400

    Removed need for cat-facts server

---
+++ js/index.js
@@ -0,0 +1,50 @@
+var colors = ['red', 'blue', 'cyan', 'green', 'orange', 'purple', 'brown'];
+
+function redoStuff() {
+    setFact();
+    setImage();
+    removeColorClasses();
+    setColorClasses();
+}
+
+function removeColorClasses() {
+    var title = document.getElementById('title');
+    var downloadButton = document.getElementById('download-button');
+    var body = document.getElementsByTagName('body')[0];
+
+    title.className = title.className.substring(0, title.className.lastIndexOf(' '));
+    downloadButton.className = downloadButton.className.substring(0, downloadButton.className.lastIndexOf(' '));
+    body.className = body.className.substring(0, body.className.lastIndexOf(' '));
+}
+
+function setLightenClasses() {
+    document.getElementById('title').className += ' text-lighten-3';
+    document.getElementById('download-button').className += ' lighten-3';
+    document.getElementsByTagName('body')[0].className += ' lighten-1';
+}
+
+function setColorClasses() {
+    var color = colors[Math.floor(Math.random()*colors.length)];
+
+    document.getElementById('title').className += ' ' + color + '-text';
+    document.getElementById('download-button').className += ' ' + color;
+    document.getElementsByTagName('body')[0].className += ' ' + color;
+}
+
+function setImage() {
+
+    var image = document.getElementById('image');
+    image.onload = function() {
+        Materialize.fadeInImage('#image');
+    };
+    document.getElementById('image').src = 'http://thecatapi.com/api/images/get?format=src&type=gif&' + (new Date()).getTime();
+}
+
+function setFact() {
+  document.getElementById('fact').innerHTML = catFacts[Math.floor(Math.random() * catFacts.length)]
+}
+
+setFact();
+setImage();
+setLightenClasses();
+setColorClasses();
